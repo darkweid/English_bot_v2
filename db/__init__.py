2eefd9d52f0a4b91fc788b4dc31fd40df31e8c7e
Mark Shamanskii <darkweid713@gmail.com>
2024-06-16 13:55:35 +0500

    добавляет реализацию функции all

--- db/__init__.py
+++ db/__init__.py
@@ -1,2 +1,4 @@
 from .managers import DatabaseManager, ExerciseManager, UserProgressManager, UserManager
 from .init import init_db
+
+__all__ = ['DatabaseManager', 'ExerciseManager', 'UserProgressManager', 'UserManager']
