b8db3dc30362886c21d5c73c25a35ebd54550a10
Mark Shamanskii <darkweid713@gmail.com>
2024-06-16 14:50:14 +0500

    актуализирует поля таблиц

--- db/models.py
+++ db/models.py
@@ -1,7 +1,4 @@
-import datetime
-
 from sqlalchemy import Column, Integer, String, ForeignKey, Date, DateTime, Time
-# from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, declarative_base
 
 Base = declarative_base()
@@ -9,6 +6,7 @@ Base = declarative_base()
 
 class GrammarExercise(Base):
     __tablename__ = 'grammar_exercises'
+    section = Column(Integer, nullable=False)
     id = Column(Integer, primary_key=True, autoincrement=True)
     russian = Column(String, nullable=False)
     english = Column(String, nullable=False)
@@ -21,30 +19,32 @@ class IrregularVerb(Base):
     english = Column(String, nullable=False)
 
 
-
 class NewWord(Base):
     __tablename__ = 'new_words'
+    section_id = Column(Integer, nullable=False)
+    section_name = Column(String, nullable=False)
     id = Column(Integer, primary_key=True, autoincrement=True)
     russian = Column(String, nullable=False)
     english = Column(String, nullable=False)
-    level = Column(Integer, nullable=False)
 
 
 class UserProgress(Base):
     __tablename__ = 'user_progress'
     user_id = Column(Integer, primary_key=True)
     exercise_type = Column(String, primary_key=True)
+    exercise_section = Column(Integer, primary_key=True)
     exercise_id = Column(Integer, primary_key=True)
     date = Column(Date, nullable=False)
 
 
 class User(Base):
     __tablename__ = 'users'
-    id = Column(Integer, autoincrement=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     registration_date = Column(DateTime)
-    user_id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, unique=True, nullable=False)
     full_name = Column(String)
     tg_login = Column(String)
     grammar_current_level = Column(Integer)
+    points = Column(Integer, default=0)
     reminder_time = Column(Time)
     time_zone = Column(String)
