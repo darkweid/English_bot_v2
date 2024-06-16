8824bfae9b5606f300973a11b75395908b4c2af0
Mark Shamanskii <darkweid713@gmail.com>
2024-06-16 14:04:37 +0500

    убирает лишние импорты

--- db/init.py
+++ db/init.py
@@ -1,16 +1,18 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.sql import text
-import asyncio
+from .models import Base
+import logging
+
+logger = logging.getLogger(__name__)
 
 DATABASE_URL = "sqlite+aiosqlite:///./english_bot.db"
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession)
-Base = declarative_base()
 
 
 async def init_db():
     async with engine.begin() as conn:
+        logger.info('Creating all tables')
         await conn.run_sync(Base.metadata.create_all)
+        logger.info('Tables created')
