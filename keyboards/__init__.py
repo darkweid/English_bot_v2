568c06686921bd5fe055b324f75baefe5ff3816c
Mark Shamanskii <darkweid713@gmail.com>
2024-06-17 21:56:13 +0500

    изменяет метод __all__ импорта клавиатур

--- keyboards/__init__.py
+++ keyboards/__init__.py
@@ -1,2 +1,5 @@
-from .keyboards import keyboard_builder, main_menu_keyboard
+from .keyboards import keyboard_builder, main_menu_keyboard, choose_section_grammar_training
 from . import set_menu as set_menu
+
+
+__all__ = ['keyboard_builder', 'main_menu_keyboard', 'choose_section_grammar_training']
