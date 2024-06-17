a74b70f9d616d1068ada410384732c6c4d7f6385
Mark Shamanskii <darkweid713@gmail.com>
2024-06-17 16:01:36 +0500

    удаляет лишние поля таблиц, добавляет доп параметры

--- db/models.py
+++ db/models.py
@@ -1,13 +1,13 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, Date, DateTime, Time
-from sqlalchemy.orm import relationship, declarative_base
+from sqlalchemy import Column, Integer, String, Date, DateTime, Time  # ,ForeignKey
+from sqlalchemy.orm import declarative_base  # ,relationship
 
 Base = declarative_base()
 
 
 class GrammarExercise(Base):
     __tablename__ = 'grammar_exercises'
-    section = Column(Integer, nullable=False)
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    section = Column(String, primary_key=True, nullable=False)
+    id = Column(Integer, primary_key=True, nullable=False)
     russian = Column(String, nullable=False)
     english = Column(String, nullable=False)
 
@@ -21,8 +21,7 @@ class IrregularVerb(Base):
 
 class NewWord(Base):
     __tablename__ = 'new_words'
-    section_id = Column(Integer, nullable=False)
-    section_name = Column(String, nullable=False)
+    section = Column(String, primary_key=True, nullable=False)
     id = Column(Integer, primary_key=True, autoincrement=True)
     russian = Column(String, nullable=False)
     english = Column(String, nullable=False)
@@ -30,10 +29,10 @@ class NewWord(Base):
 
 class UserProgress(Base):
     __tablename__ = 'user_progress'
-    user_id = Column(Integer, primary_key=True)
-    exercise_type = Column(String, primary_key=True)
-    exercise_section = Column(Integer, primary_key=True)
-    exercise_id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, primary_key=True, nullable=False)
+    exercise_type = Column(String, primary_key=True, nullable=False)
+    exercise_section = Column(Integer, primary_key=True, nullable=True)
+    exercise_id = Column(Integer, primary_key=True, nullable=False)
     date = Column(Date, nullable=False)
 
 
@@ -44,7 +43,6 @@ class User(Base):
     user_id = Column(Integer, unique=True, nullable=False)
     full_name = Column(String)
     tg_login = Column(String)
-    grammar_current_level = Column(Integer)
     points = Column(Integer, default=0)
     reminder_time = Column(Time)
     time_zone = Column(String)
