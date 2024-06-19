72413b1c8ad308a0e03bb5a31822ac0a91db860a
Mark Shamanskii <darkweid713@gmail.com>
2024-06-19 22:45:04 +0500

    добавляет Enum кнопок для тестов по английскому

--- lexicon/__init__.py
+++ lexicon/__init__.py
@@ -1,1 +1,3 @@
-from .lexicon_ru import BasicButtons, MainMenuButtons, AdminMenuButtons, TestingSections, TestingSubsections, MessageTexts
+from .lexicon_ru import (BasicButtons, MainMenuButtons, AdminMenuButtons, TestingSections, MessageTexts, TensesSections,
+                         ConstructionsSections, PhrasesAndWordsSections, PrepositionsSections, ModalVerbsSections,
+                         ConditionalsSections, testing_section_mapping)
