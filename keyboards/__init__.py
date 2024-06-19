41f80fae22ad2cbc6326ddd4760186259ae4751e
Mark Shamanskii <darkweid713@gmail.com>
2024-06-19 22:45:43 +0500

    убирает из упрощенного импорта клавиатуру choose_subsection_testing

--- keyboards/__init__.py
+++ keyboards/__init__.py
@@ -1,4 +1,4 @@
-from .keyboards import keyboard_builder, main_menu_keyboard, choose_section_testing, choose_subsection_testing
+from .keyboards import keyboard_builder, main_menu_keyboard, choose_section_testing_keyboard
 from . import set_menu as set_menu
 
-__all__ = ['keyboard_builder', 'main_menu_keyboard', 'choose_section_testing', 'choose_subsection_testing']
+__all__ = ['keyboard_builder', 'main_menu_keyboard', 'choose_section_testing_keyboard']
