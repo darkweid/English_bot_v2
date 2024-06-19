8e560623eaf58b7e579a372623ecbef5cb2edb81
Mark Shamanskii <darkweid713@gmail.com>
2024-06-19 11:37:52 +0500

    переименовывает состояния пользователей

--- states/states.py
+++ states/states.py
@@ -1,14 +1,12 @@
 from aiogram.filters.state import State, StatesGroup
 
-class TestFSM(StatesGroup):
-    test = State()
 
 class LearningFSM(StatesGroup):
     default = State()
     choose_type_of_exercise = State()
-    grammar_training = State() # Пользователь проходит тренажер по грамматике
-    grammar_choosing_section = State()# Пользователь выбирает раздел в тренажере по грамматике
-    grammar_choosed_section = State()# Пользователь выбрал раздел в тренажере по грамматике
+    testing = State()  # Пользователь проходит тренажер по грамматике
+    testing_choosing_section = State()  # Пользователь выбирает раздел в тренажере по грамматике
+    testing_choosed_section = State()  # Пользователь выбрал раздел в тренажере по грамматике
 
     verbs_learning = State()  # Пользователь изучает неправильные глаголы
     new_words_learning = State()  # Пользователь изучает новые слова
@@ -17,10 +15,13 @@ class LearningFSM(StatesGroup):
 
 class AdminFSM(StatesGroup):
     default = State()  # Начальное состояние
-    choose_type_of_exercise_grammar = State()
-    choose_what_to_do_with_exercise_grammar = State()
-    adding_sentence_grammar = State()
-    deleting_exercise_grammar = State()
+    choose_section_testing = State()
+    choose_subsection_testing = State()
+    choose_management_action_testing = State()
+    adding_exercise_testing = State()
+    deleting_exercise_testing = State()
+    editing_exercise_testing = State()
+
     manage_users = State()  # Управление пользователями
     view_reports = State()  # Просмотр отчетов
     configure_settings = State()  # Настройка параметров
