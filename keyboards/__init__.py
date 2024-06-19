ef85fe792d8106604eaac30c2d70e7e51397cc2b
Mark Shamanskii <darkweid713@gmail.com>
2024-06-19 11:42:46 +0500

    добавляет новые клавиатуры в упрощенный импорт

--- keyboards/__init__.py
+++ keyboards/__init__.py
@@ -1,5 +1,4 @@
-from .keyboards import keyboard_builder, main_menu_keyboard, choose_section_grammar_training
+from .keyboards import keyboard_builder, main_menu_keyboard, choose_section_testing, choose_subsection_testing
 from . import set_menu as set_menu
 
-
-__all__ = ['keyboard_builder', 'main_menu_keyboard', 'choose_section_grammar_training']
+__all__ = ['keyboard_builder', 'main_menu_keyboard', 'choose_section_testing', 'choose_subsection_testing']
