42eaab83e67bf0ff45335d32c0c085a9e0066808
Mark Shamanskii <darkweid713@gmail.com>
2024-06-27 14:13:35 +0500

    добавляет функцию планировщика рассылок пользователям
    использует хранилище SQlite для защиты потери данных о рассылках при перезапуске бота

--- utils/scheduling.py
+++ utils/scheduling.py
@@ -1,20 +1,29 @@
-from aiogram import Bot
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from datetime import timedelta, timezone
+from apscheduler.jobstores.memory import MemoryJobStore
+from datetime import timedelta, timezone, datetime
 
+from apscheduler.triggers.date import DateTrigger
+from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 
-from utils import send_message_to_user
+from utils import send_message_to_user, send_message_to_all_users
 from db import UserManager
 from lexicon import MessageTexts
 
 user_manager = UserManager()
-scheduler = AsyncIOScheduler()
+
+jobstores = {
+    'reminders': MemoryJobStore(),
+    'broadcasts': SQLAlchemyJobStore(url='sqlite:///jobs.sqlite')
+}
+
+scheduler = AsyncIOScheduler(jobstores=jobstores)
 
 
-async def schedule_reminders(bot: Bot):
+async def schedule_reminders():
     users = await user_manager.get_all_users()
-    scheduler.remove_all_jobs()
+    scheduler.remove_all_jobs(jobstore='reminders')
     for user in users:
+        username = user.get('tg_login')
         reminder_time = user.get('reminder_time')
         user_tz_offset = user.get('time_zone')
         user_id = user.get('user_id')
@@ -23,4 +32,18 @@ async def schedule_reminders(bot: Bot):
             scheduler.add_job(func=send_message_to_user, trigger='cron',
                               hour=reminder_time.hour, minute=reminder_time.minute,
                               timezone=user_tz,
-                              kwargs={'user_id': user_id, 'bot': bot, 'text': MessageTexts.REMINDER.value})
+                              kwargs={'user_id': user_id, 'text': MessageTexts.REMINDER.value},
+                              jobstore='reminders', name=f'Reminder for @{username} ({user_id})')
+
+
+async def schedule_broadcast(date_time: datetime, text: str):
+    tz = timezone(timedelta(hours=3))
+    date_time = date_time.replace(tzinfo=tz)
+    trigger = DateTrigger(run_date=date_time)
+    scheduler.add_job(func=send_message_to_all_users, trigger=trigger,
+                      kwargs={'text': text},
+                      jobstore='broadcasts', name=f'Broadcast {date_time.strftime("%H:%M (UTC+3) %d.%m.%Y")}')
+
+
+async def delete_scheduled_broadcasts():
+    scheduler.remove_all_jobs(jobstore='broadcasts')
