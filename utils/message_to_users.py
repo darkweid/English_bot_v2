8373d2f2adb8cd0e760421c4139139a6e484a252
Mark Shamanskii <darkweid713@gmail.com>
2024-06-27 14:15:04 +0500

    добавляет получение экземпляра бота для отправки сообщений прямо в эти функции
    связано с невозможностью БД планировщика хранить экземляр бота

--- utils/message_to_users.py
+++ utils/message_to_users.py
@@ -1,18 +1,23 @@
 from config_data.config import Config, load_config
 from aiogram import Bot
 from db import UserManager
+from .bot_init import get_bot_instance
 
 config: Config = load_config()
 ADMINS: list = config.tg_bot.admin_ids
 user_manager: UserManager = UserManager()
 
 
-async def send_message_to_all_users(bot: Bot, text: str):
-    users = user_manager.get_all_users()
+async def send_message_to_all_users(text: str):
+    bot: Bot = await get_bot_instance()
+    if bot is None:
+        raise Exception('Bot instance is not available')
+    users = await user_manager.get_all_users()
     for user in users:
         user_id = user.get('user_id')
         await bot.send_message(user_id, text=text)
 
 
-async def send_message_to_user(bot: Bot, user_id: int, text: str):
+async def send_message_to_user(user_id: int, text: str):
+    bot: Bot = await get_bot_instance()
     await bot.send_message(user_id, text=text)
