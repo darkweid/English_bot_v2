a06fa0f24960b449936abf15bf1d0f97b48213ab
Mark Shamanskii <darkweid713@gmail.com>
2024-07-03 20:00:29 +0500

    добавляет UserWordsLearningManager в упрощенный импорт

--- db/__init__.py
+++ db/__init__.py
@@ -1,4 +1,6 @@
-from .managers import DatabaseManager, ExerciseManager, UserProgressManager, UserManager
+from .managers import (DatabaseManager, ExerciseManager, UserProgressManager, UserManager, NewWordsExerciseManager,
+                       UserWordsLearningManager)
 from .init import init_db
 
-__all__ = ['DatabaseManager', 'ExerciseManager', 'UserProgressManager', 'UserManager']
+__all__ = ['DatabaseManager', 'ExerciseManager', 'UserProgressManager', 'UserManager', 'NewWordsExerciseManager',
+           'UserWordsLearningManager']
