b41c4be72e50341719164a1e9a8849f8dce9aa19
Mark Shamanskii <darkweid713@gmail.com>
2024-08-08 22:10:07 +0500

    Add suggestion to include more words for user study in reminder if user has few words in active learning

--- utils/message_to_users.py
+++ utils/message_to_users.py
@@ -62,16 +62,24 @@ async def send_reminder_to_user(user_id: int):
     - user_id (int): The ID of the user.
     """
     bot: Bot = await get_bot_instance()
-    count_words = await user_words_manager.get_count_all_exercises_for_today_by_user(user_id=user_id)
+    count_words_for_today = await user_words_manager.get_count_all_exercises_for_today_by_user(user_id=user_id)
+    active_learning_count = await user_words_manager.get_count_active_learning_exercises(user_id=user_id)
     try:
-        if count_words > 0:
-            word_form = await get_word_declension(count_words)
+        if count_words_for_today > 0:
+            word_form = await get_word_declension(count_words_for_today)
             await bot.send_message(user_id, text=MessageTexts.REMINDER_WORDS_TO_LEARN.value.format(word_form),
                                    reply_markup=await keyboard_builder(1,
                                                                        learn_new_words=BasicButtons.LEARN_ADDED_WORDS)
                                    )
-        else:
-            await bot.send_message(user_id, text=MessageTexts.REMINDER.value)
+        elif count_words_for_today == 0 and active_learning_count < 12:
+            await bot.send_message(user_id,
+                                   text=f'{MessageTexts.REMINDER.value}\n{MessageTexts.ADVICE_TO_ADD_MORE_WORDS.value}',
+                                   reply_markup=await keyboard_builder(1,
+                                                                       BasicButtons.MAIN_MENU,
+                                                                       add_new_words=BasicButtons.ADD_WORDS, args_go_first=False))
+        elif count_words_for_today == 0 and active_learning_count > 12:
+            await bot.send_message(user_id, text=MessageTexts.REMINDER.value,
+                                   reply_markup=await keyboard_builder(1, BasicButtons.MAIN_MENU))
     except TelegramForbiddenError as e:
         logger.error(f'Failed to send message to user {user_id}:\n{e}')
 
