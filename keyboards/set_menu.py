d3d63e7c206c89867c5a047b494dd2781730c48c
Mark Shamanskii <darkweid713@gmail.com>
2024-08-11 13:55:40 +0500

    Remove unused commands in menu

--- keyboards/set_menu.py
+++ keyboards/set_menu.py
@@ -4,10 +4,10 @@ from aiogram.types import BotCommand
 
 async def set_main_menu(bot: Bot):
     main_menu_commands = [
-        BotCommand(command='/reset_fsm', description='Cброс машины состояний'),
-        BotCommand(command='/main_menu', description='Главное меню'),
+        # BotCommand(command='/reset_fsm', description='Cброс машины состояний'),
         BotCommand(command='/start', description='Перезапустить бота'),
-        BotCommand(command='/stats', description='Посмотреть мою статистику'),
+        BotCommand(command='/main_menu', description='Главное меню'),
+        # BotCommand(command='/stats', description='Посмотреть мою статистику'),
         BotCommand(command='/reminder', description='Установить напоминания'),
         BotCommand(command='/info', description='Как пользоваться ботом'),
         BotCommand(command='/admin', description='Административная панель')
