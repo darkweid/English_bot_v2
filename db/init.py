3f53493f94ed6db2ed737a2b6d04ac7325dc99c1
Mark Shamanskii <darkweid713@gmail.com>
2024-08-13 17:30:06 +0500

    Add exception handling during database initialization

--- db/init.py
+++ db/init.py
@@ -7,7 +7,6 @@ logger = logging.getLogger(__name__)
 # DATABASE_URL = "sqlite+aiosqlite:///./english_bot.db"
 DATABASE_URL = "postgresql+asyncpg://superuser:superpassword@postgres:5432/data"
 
-
 engine = create_async_engine(DATABASE_URL, echo=True)
 SessionLocal = async_sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
@@ -15,5 +14,8 @@ SessionLocal = async_sessionmaker(autocommit=False, autoflush=False, bind=engine
 async def init_db():
     async with engine.connect() as conn:
         logger.info('Creating all tables')
-        await conn.run_sync(Base.metadata.create_all)
-        logger.info('Tables created')
+        try:
+            await conn.run_sync(Base.metadata.create_all)
+            logger.info('Tables created')
+        except Exception as e:
+            logger.error(f"Error occurred: {e}")
