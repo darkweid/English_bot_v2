458a64f60791031e7ef87d793ad0ca1b03877eeb
Mark Shamanskii <darkweid713@gmail.com>
2024-08-13 19:32:11 +0500

    Change transaction method

--- db/init.py
+++ db/init.py
@@ -11,7 +11,7 @@ SessionLocal = async_sessionmaker(autocommit=False, autoflush=False, bind=engine
 
 
 async def init_db():
-    async with engine.connect() as conn:
+    async with engine.begin() as conn:
         logger.info('Creating all tables')
         try:
             await conn.run_sync(Base.metadata.create_all)
