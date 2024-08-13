e0158dee96d182d96b6e11cbb12b3450b1bfd63d
Mark Shamanskii <darkweid713@gmail.com>
2024-08-13 13:35:00 +0500

    test

--- db/models.py
+++ db/models.py
@@ -12,61 +12,61 @@ class Base(DeclarativeBase):
 class TestingExercise(Base):
     __tablename__ = 'testing_exercises'
     section: Mapped[str] = mapped_column(String, primary_key=True, nullable=False)
-    # subsection: Mapped[str] = mapped_column(String, primary_key=True, nullable=False)
-    # id: Mapped[int] = mapped_column(Integer, primary_key=True, nullable=False)
-    # test: Mapped[str] = mapped_column(String, nullable=False)
-    # answer: Mapped[str] = mapped_column(String, nullable=False)
+    subsection: Mapped[str] = mapped_column(String, primary_key=True, nullable=False)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, nullable=False)
+    test: Mapped[str] = mapped_column(String, nullable=False)
+    answer: Mapped[str] = mapped_column(String, nullable=False)
 
 
 class NewWords(Base):
     __tablename__ = 'new_words'
     section: Mapped[str] = mapped_column(String, primary_key=True, nullable=False)
-    # subsection: Mapped[str] = mapped_column(String, primary_key=True, nullable=False)
-    # id: Mapped[int] = mapped_column(Integer, primary_key=True, nullable=False)
-    # russian: Mapped[str] = mapped_column(String, nullable=False)
-    # english: Mapped[str] = mapped_column(String, nullable=False)
-    # user_words: Mapped[List["UserWordsLearning"]] = relationship(
-    #     back_populates="new_word"
-    # )
+    subsection: Mapped[str] = mapped_column(String, primary_key=True, nullable=False)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, nullable=False)
+    russian: Mapped[str] = mapped_column(String, nullable=False)
+    english: Mapped[str] = mapped_column(String, nullable=False)
+    user_words: Mapped[List["UserWordsLearning"]] = relationship(
+        back_populates="new_word"
+    )
 
 
 class UserWordsLearning(Base):
     __tablename__ = 'user_words_learning'
     user_id: Mapped[int] = mapped_column(BigInteger, primary_key=True, nullable=False, index=True)
-#     section: Mapped[str] = mapped_column(String, primary_key=True, nullable=False, index=True)
-#     subsection: Mapped[str] = mapped_column(String, primary_key=True, nullable=False, index=True)
-#     exercise_id: Mapped[int] = mapped_column(Integer, primary_key=True, nullable=False)
-#     attempts: Mapped[int] = mapped_column(Integer, default=0, nullable=False)
-#     success: Mapped[int] = mapped_column(Integer, default=0, nullable=False)
-#     next_review_date: Mapped[date] = mapped_column(Date, default=date.today, nullable=False)
-#     add_date: Mapped[date] = mapped_column(Date, default=date.today, nullable=False)
-#
-#     new_word: Mapped["NewWords"] = relationship(
-#         back_populates="user_words"
-#     )
-#
-#     __table_args__ = (
-#         ForeignKeyConstraint(
-#             ['section', 'subsection', 'exercise_id'],
-#             ['new_words.section', 'new_words.subsection', 'new_words.id']
-#         ),
-#     )
-#
-#
-# Index('ix_user_section', UserWordsLearning.user_id, UserWordsLearning.section,
-#       UserWordsLearning.subsection)
+    section: Mapped[str] = mapped_column(String, primary_key=True, nullable=False, index=True)
+    subsection: Mapped[str] = mapped_column(String, primary_key=True, nullable=False, index=True)
+    exercise_id: Mapped[int] = mapped_column(Integer, primary_key=True, nullable=False)
+    attempts: Mapped[int] = mapped_column(Integer, default=0, nullable=False)
+    success: Mapped[int] = mapped_column(Integer, default=0, nullable=False)
+    next_review_date: Mapped[date] = mapped_column(Date, default=date.today, nullable=False)
+    add_date: Mapped[date] = mapped_column(Date, default=date.today, nullable=False)
+
+    new_word: Mapped["NewWords"] = relationship(
+        back_populates="user_words"
+    )
+
+    __table_args__ = (
+        ForeignKeyConstraint(
+            ['section', 'subsection', 'exercise_id'],
+            ['new_words.section', 'new_words.subsection', 'new_words.id']
+        ),
+    )
+
+
+Index('ix_user_section', UserWordsLearning.user_id, UserWordsLearning.section,
+      UserWordsLearning.subsection)
 
 
 class UserProgress(Base):
     __tablename__ = 'user_progress'
     user_id: Mapped[int] = mapped_column(BigInteger, primary_key=True, nullable=False)
-    # exercise_type: Mapped[str] = mapped_column(String, primary_key=True, nullable=False)
-    # exercise_section: Mapped[int] = mapped_column(Integer, primary_key=True, nullable=True)
-    # exercise_subsection: Mapped[int] = mapped_column(Integer, primary_key=True, nullable=True)
-    # exercise_id: Mapped[int] = mapped_column(Integer, primary_key=True, nullable=False)
-    # attempts: Mapped[int] = mapped_column(Integer, default=1)
-    # success: Mapped[bool] = mapped_column(Boolean, default=False)
-    # date: Mapped[date] = mapped_column(Date, nullable=False)
+    exercise_type: Mapped[str] = mapped_column(String, primary_key=True, nullable=False)
+    exercise_section: Mapped[int] = mapped_column(Integer, primary_key=True, nullable=True)
+    exercise_subsection: Mapped[int] = mapped_column(Integer, primary_key=True, nullable=True)
+    exercise_id: Mapped[int] = mapped_column(Integer, primary_key=True, nullable=False)
+    attempts: Mapped[int] = mapped_column(Integer, default=1)
+    success: Mapped[bool] = mapped_column(Boolean, default=False)
+    date: Mapped[date] = mapped_column(Date, nullable=False)
 
 
 class User(Base):
@@ -84,10 +84,10 @@ class User(Base):
 class DailyStatistics(Base):
     __tablename__ = 'daily_statistics'
     date: Mapped[date] = mapped_column(Date, primary_key=True, nullable=False)
-    # total_testing_exercises: Mapped[int] = mapped_column(Integer, default=0, nullable=False)
-    # total_new_words: Mapped[int] = mapped_column(Integer, default=0, nullable=False)
-    # total_irregular_verbs: Mapped[int] = mapped_column(Integer, default=0, nullable=False)
-    # new_users: Mapped[int] = mapped_column(Integer, default=0, nullable=False)
+    total_testing_exercises: Mapped[int] = mapped_column(Integer, default=0, nullable=False)
+    total_new_words: Mapped[int] = mapped_column(Integer, default=0, nullable=False)
+    total_irregular_verbs: Mapped[int] = mapped_column(Integer, default=0, nullable=False)
+    new_users: Mapped[int] = mapped_column(Integer, default=0, nullable=False)
 
 
-# Index('ix_daily_statistics_date', DailyStatistics.date)
+Index('ix_daily_statistics_date', DailyStatistics.date)
