8e1c4f735fe1e6c9f6a3d6b385803fcb91ad4fa2
Mark Shamanskii <darkweid713@gmail.com>
2024-10-28 12:56:41 +0500

    Refactor: Consolidate router registration into a single method

--- main.py
+++ main.py
@@ -2,17 +2,16 @@ import asyncio
 import logging
 
 from aiogram import Bot, Dispatcher
-from aiogram.fsm.storage.redis import RedisStorage, Redis
+from aiogram.fsm.storage.redis import RedisStorage
 from config_data.config import Config, load_config
 from handlers import *
 from keyboards.set_menu import set_main_menu
 from utils import send_message_to_admin, scheduler, schedule_reminders, init_bot_instance, get_bot_instance
 from db import init_db
 
-logging.basicConfig(  # filename='bot.log',
-    level=logging.INFO,
-    format='#%(levelname)-8s '
-           '[%(asctime)s] - %(name)s - %(message)s')
+logging.basicConfig(level=logging.INFO,
+                    format='#%(levelname)-8s '
+                           '[%(asctime)s] - %(name)s - %(message)s')
 logger = logging.getLogger(__name__)
 sqlalchemy_logger = logging.getLogger('sqlalchemy.engine')
 sqlalchemy_logger.setLevel(logging.INFO)
@@ -27,26 +26,15 @@ async def main():
     try:
         logger.info('Starting bot')
 
-        # redis: Redis = Redis(host='localhost')
-        # storage: RedisStorage = RedisStorage(redis=redis)
         storage = RedisStorage.from_url(url=REDIS_DSN)
-
         await init_db()
         await init_bot_instance(token=BOT_TOKEN)
-
         bot: Bot = await get_bot_instance()
         dp: Dispatcher = Dispatcher(storage=storage)
 
-        dp.include_router(user_commands_router)
-        dp.include_router(admin_router)
-
-        dp.include_router(user_navigation_router)
-        dp.include_router(user_stats_router)
-        dp.include_router(user_reminder_router)
-        dp.include_router(user_testing_router)
-        dp.include_router(user_irr_verbs_router)
-        dp.include_router(user_new_words_router)
-        dp.include_router(fallback_router)
+        dp.include_routers(user_commands_router, admin_router, user_navigation_router, user_stats_router,
+                           user_reminder_router, user_testing_router, user_irr_verbs_router, user_new_words_router,
+                           fallback_router)
 
         await set_main_menu(bot)
         await bot.delete_webhook(drop_pending_updates=True)
