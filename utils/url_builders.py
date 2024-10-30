971cc2c02ba05662f61c96f8a4267319ae3e311d
Mark Shamanskii <darkweid713@gmail.com>
2024-10-30 12:41:38 +0500

    Feat: Add function to return a formatted link with embedded word text

--- utils/url_builders.py
+++ utils/url_builders.py
@@ -11,3 +11,18 @@ def youglish_url_builder(word: str) -> str:
     word = ' '.join(word.split()).replace(' ', '%20')
 
     return f'https://www.youglish.com/pronounce/{word}/english'
+
+
+def word_with_youglish_link(word_english: str) -> str:
+    """
+    Creates an HTML hyperlink to YouGlish pronunciation for the given word.
+
+    The hyperlink uses YouGlish's URL structure for English pronunciation.
+
+    Args:
+        word_english (str): The word to link to YouGlish.
+
+    Returns:
+        str: An HTML anchor element as a string.
+    """
+    return f'<a href="{youglish_url_builder(word_english)}">{word_english.capitalize()}</a>'
